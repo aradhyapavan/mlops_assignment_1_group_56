1dda71231028634eb259d9e64b2fc0b3f6694624
Aradhya Pavan H S <39955465+aradhyapavan@users.noreply.github.com>
2024-08-06 13:13:40 +0530

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,17 +1,21 @@
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
 from joblib import load
 import pandas as pd
 import mlflow
 
+# Paths for loading and saving data
 test_path = 'test_output'
 output_path = 'final_output'
-mlflow.set_experiment(experiment_id="559978265238575036")
+
+# Set up MLflow tracking
 mlflow.set_tracking_uri('http://127.0.0.1:5000/')
 mlflow.autolog()
 
+experiment_name = "liver_disease_prediction"
+mlflow.set_experiment(experiment_name)
 
 # Load test data and model
-X_test = pd.read_csv(test_path+'/X_test.csv')
+X_test = pd.read_csv(test_path + '/X_test.csv')
 
 # Remove the 'Unnamed: 0' column if it exists
 if 'Unnamed: 0' in X_test.columns:
@@ -21,18 +25,16 @@ model = load('models/model.joblib')
 
 # Make predictions
 y_pred = model.predict(X_test)
-print(y_pred)
-with mlflow.start_run() as run:
+
+with mlflow.start_run():
     accuracy = accuracy_score(y_pred, model.predict(X_test))
     mlflow.log_metric('accuracy', accuracy)
-    mlflow.log_param('n_estimators', model.n_estimators)
+
 # Convert y_pred to DataFrame
 y_pred_df = pd.DataFrame(y_pred, columns=['Prediction'])
 
 # Combine X_test and y_pred_df
 combined_df = pd.concat([X_test, y_pred_df], axis=1)
-print(combined_df)
-# Save the combined DataFrame to a CSV file
-combined_df.to_csv(output_path+'/final_predicted_output.csv', index=False)
-combined_df.to_csv('output/final_predicted_output.csv', index=False)
 
+# Save the combined DataFrame to a CSV file
+combined_df.to_csv(output_path + '/final_predicted_output.csv', index=False)
